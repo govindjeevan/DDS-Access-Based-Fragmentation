c15ade7a810c6f8b02faa6f127edf290f41d1b4e
smarty1palak <smarty1palak@gmail.com>
2019-11-08 00:19:20 +0530

    read/write volume incomplete

--- app/models/site_y_access_log.rb
+++ app/models/site_y_access_log.rb
@@ -3,13 +3,13 @@ class SiteYAccessLog < SiteYRecord
 
   def self.update_read_log(record)
     if record.class != SiteYAccessLog
-      CentralAccessLog.create(site_id: record.id, rw: 0, fragment_id: 1, volume: 1)
+      SiteYAccessLog.create(site_id: record.id, rw: 0, fragment_id: 1, volume: 1)
     end
   end
 
   def self.update_write_log(record)
     if record.class != SiteYAccessLog
-      CentralAccessLog.create(site_id: record.id, rw: 1, fragment_id: 1, volume: 1)
+      SiteYAccessLog.create(site_id: record.id, rw: 1, fragment_id: 1, volume: 1)
     end
   end
 
