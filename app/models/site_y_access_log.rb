e36672b496d53e87d0b1b9e988a620a1a795d0c2
smarty1palak <smarty1palak@gmail.com>
2019-11-07 23:25:12 +0530

    mistake corrected in fragment and site id

--- app/models/site_y_access_log.rb
+++ app/models/site_y_access_log.rb
@@ -3,13 +3,13 @@ class SiteYAccessLog < SiteYRecord
 
   def self.update_read_log(record)
     if record.class != SiteYAccessLog
-      CentralAccessLog.create(site_id: 0, rw: 0, fragment_id: record.id, volume: 1)
+      CentralAccessLog.create(site_id: record.id, rw: 0, fragment_id: 1, volume: 1)
     end
   end
 
   def self.update_write_log(record)
     if record.class != SiteYAccessLog
-      CentralAccessLog.create(site_id: 0, rw: 1, fragment_id: record.id, volume: 1)
+      CentralAccessLog.create(site_id: record.id, rw: 1, fragment_id: 1, volume: 1)
     end
   end
 
