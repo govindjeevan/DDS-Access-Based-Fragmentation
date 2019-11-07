4b839fd206694d507ded04aa9c38e2ec1ac802c5
smarty1palak <smarty1palak@gmail.com>
2019-11-07 23:21:35 +0530

    read and write for sites x,y,z

--- app/models/site_y_access_log.rb
+++ app/models/site_y_access_log.rb
@@ -1,3 +1,16 @@
 class SiteYAccessLog < SiteYRecord
   self.table_name = "access_log"
+
+  def self.update_read_log(record)
+    if record.class != SiteYAccessLog
+      CentralAccessLog.create(site_id: 0, rw: 0, fragment_id: record.id, volume: 1)
+    end
+  end
+
+  def self.update_write_log(record)
+    if record.class != SiteYAccessLog
+      CentralAccessLog.create(site_id: 0, rw: 1, fragment_id: record.id, volume: 1)
+    end
+  end
+
 end
