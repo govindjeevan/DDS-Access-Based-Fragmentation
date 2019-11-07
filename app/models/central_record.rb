d9e333d48966c04b9d4b3a2ee307380963a5119c
Pavan Vachhani <vachhanihpavan@gmail.com>
2019-11-07 13:35:08 +0530

    Added callbacks for Write/Read

--- app/models/central_record.rb
+++ app/models/central_record.rb
@@ -2,4 +2,12 @@ class CentralRecord < ApplicationRecord
   self.abstract_class = true
 
   establish_connection :central
+
+  after_find do |record|
+    puts "Read #{record.to_json}!"
+  end
+
+  after_commit do |record|
+    puts "Write #{record.to_json}!"
+  end
 end
---
+++ db/migrate/20191107074933_add_access_logs_to_site_xdb.rb
@@ -0,0 +1,18 @@
+class AddAccessLogsToSiteXdb < ActiveRecord::Migration[5.2]
+  def connection
+    @connection = ActiveRecord::Base.establish_connection(:site_x).connection
+  end
+
+  def change
+    create_table :access_log do |t|
+      t.integer :site_id
+      t.boolean :rw
+      t.integer :fragment_id
+      t.integer :volume
+
+      t.timestamps
+    end
+
+    @connection = ActiveRecord::Base.establish_connection(:central).connection
+  end
+end
