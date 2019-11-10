36ce35a464efdc2bdcca8315ac161e77f66248c8
Govind Jeevan <govindjeevan7@gmail.com>
2019-11-10 18:54:38 +0530

    migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "movies/local", :action => "local_movies", :controller => "application"
+  get "movies", :action => "all_movies", :controller => "application"
+  get "movies/year/:year", :action => "movies_by_year", :controller => "application"
+
 end
