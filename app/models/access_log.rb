5573a80b908d75371e64108bfe652a8cc124d17a
Govind Jeevan <govindjeevan7@gmail.com>
2019-11-10 18:55:15 +0530

    synchronizing scattered fragmenets

--- app/models/access_log.rb
+++ app/models/access_log.rb
@@ -1,15 +1,11 @@
 class AccessLog < Movie
   self.table_name = 'access_log'
 
-  def self.update_read_log(record)
-    unless record.class.to_s.include? 'AccessLog'
-      AccessLog.create(site_id: record.id, rw: 0, fragment_id: 2, volume: 1)
-    end
+  def self.create_read_log(fragment_id, volume)
+      AccessLog.create(site_id: @site, rw: 0, fragment_id: fragment_id, volume: volume)
   end
 
-  def self.update_write_log(record)
-    unless record.class.to_s.include? 'AccessLog'
-      AccessLog.create(site_id: record.site.id, rw: 1, fragment_id: 2, volume: 1)
-    end
+  def self.create_write_log(fragment_id, volume)
+      AccessLog.create(site_id: @site, rw: 1, fragment_id: fragment_id, volume: volume)
   end
 end
--- app/models/query_router.rb
+++ app/models/query_router.rb
@@ -1,4 +1,57 @@
 class QueryRouter < ApplicationRecord
   self.table_name = 'fragment_site_data'
 
+
+  def self.find_site_of_fragment(fragment_id)
+    establish_connection(:central)
+    fragment_detail = QueryRouter.where(fragment_id: fragment_id).last
+    if fragment_detail
+      return fragment_detail.site_id
+    else
+      return nil
+    end
+  end
+
+  def self.update_query_router(fragment_id, site_id)
+    establish_connection(:central)
+    QueryRouter.first_or_create(fragment_id).update(site_id: site_id)
+    ApplicationRecord.establish_connection_to_site(@site)
+  end
+
+  def self.relocate_fragment(fragment_id, target_site_id)
+    site_id = QueryRouter.find_by_fragment_id(fragment_id).try(:site_id)
+
+    ApplicationRecord.establish_connection_to_site(site_id)
+
+    fragment = Movie.find_by_year(fragment_id).to_a
+    update_query_router(fragment_id, target_site_id)
+  end
+
+  def self.sync_scattered_fragment(fragment_id)
+    establish_connection(:central)
+    target_site_id = QueryRouter.find_by_fragment_id(fragment_id).try(:site_id)
+    if target_site_id
+      fragment = []
+      (1..3).to_a.each do |site_id|
+        ApplicationRecord.establish_connection_to_site(site_id)
+        movies = Movie.find_by_year(fragment_id)
+        fragment.push(movies.to_a)
+        movies.delete_all
+      end
+      ApplicationRecord.establish_connection_to_site(target_site_id)
+      fragment.each do |fragment_piece|
+        fragment_piece.each do |movie|
+          Movie.create(:title => movie.title, :release_date => movie.release_date)
+        end
+      end
+    end
+  end
+
+
+  def self.sync_all
+    QueryRouter.all.each do |x|
+      sync_scattered_fragment(x.fragment_id)
+    end
+  end
+
 end
